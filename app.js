b152399bc69d507dfaeb05568d9571cfc3e25a6c
Eduardo M. Cabezas <edu3cabezas@gmail.com>
2023-11-25 15:31:44 -0300

    server init

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const app = express();
-const PORT = 3003;
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = 3001;
